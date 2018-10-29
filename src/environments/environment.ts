d9fa54458734a2c4da9385f3b621aede69c6a4b0
Lucas OMS <lucas.oms@etu.unice.fr>
2018-10-29 23:20:11 +0100

    Ajout du fichier d'environnements pour la BD de développement

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Lucas OMS on 30/08/2018.
+ */
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyAjfhqoRNbO-CFZVdB-ax8QWoP3WMuY-0s',
+        authDomain: 'site-asso-pns.firebaseapp.com',
+        databaseURL: 'https://site-asso-pns.firebaseio.com',
+        projectId: 'site-asso-pns',
+        storageBucket: 'site-asso-pns.appspot.com',
+        messagingSenderId: '797343092119'
+    }
+};
